9f98ad4fb81d5d161333e0f0a8e9823b34d2fd4a
Adrian <yenlhs@gmail.com>
2023-10-18 11:53:45 +1100

    added pagination in postlist

--- src/apollo/Client.tsx
+++ src/apollo/Client.tsx
@@ -4,20 +4,20 @@ import { ApolloClient, InMemoryCache, ApolloProvider, TypePolicies } from '@apol
 const typePolicies: TypePolicies = {
 	Query: {
 		fields: {
-			postPaginatedList:{
+			postPaginatedList: {
 				keyArgs: false,
-				merge(existing, incoming){
-					return [...existing, ...incoming]
-				}
-			}
-		}
-	}
-}
+				merge(existing = [], incoming) {
+					return [...existing, ...incoming];
+				},
+			},
+		},
+	},
+};
 
 const client = new ApolloClient({
 	uri: 'https://massaranduba.stepzen.net/api/ordered-numbat/__graphql',
 	headers: { Authorization: 'apikey massaranduba::stepzen.io+1000::2509336b30dfec9f4d39e257faa05272ec256cc160622db9c0ca1aa087caf5a0' },
-	cache: new InMemoryCache(),
+	cache: new InMemoryCache({ typePolicies }),
 });
 
 export default client;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,10 +5,11 @@ import { FlatList } from 'react-native-gesture-handler';
 
 import { gql, useQuery } from '@apollo/client';
 import { ActivityIndicator } from 'react-native';
+import { useState } from 'react';
 
-const postList = gql`
-	query PostListQuery {
-		postList {
+const postPaginatedList = gql`
+	query PostPaginatedListQuery($first: Int, $after: Int) {
+		postPaginatedList(first: $first, after: $after) {
 			id
 			content
 			image
@@ -25,8 +26,12 @@ const postList = gql`
 const firstPost = posts[0];
 
 export default function HomeFeedScreen() {
-	const { loading, error, data } = useQuery(postList);
-	console.log(data);
+	const [hasMore, setHasMore] = useState(true);
+	const { loading, error, data, fetchMore, refetch } = useQuery(postPaginatedList, {
+		variables: {
+			first: 2,
+		},
+	});
 
 	if (loading) {
 		return <ActivityIndicator />;
@@ -35,12 +40,29 @@ export default function HomeFeedScreen() {
 		console.log(error);
 		return <Text>Something went wrong!</Text>;
 	}
+
+	const loadMore = async () => {
+		if (!hasMore) {
+			return;
+		}
+		console.log('load more');
+		const res = await fetchMore({
+			variables: {
+				after: data.postPaginatedList.length,
+			},
+		});
+
+		if (res.data.postPaginatedList.length === 0) setHasMore(false);
+	};
 	return (
 		<FlatList
-			data={data.postList}
+			data={data.postPaginatedList}
 			renderItem={({ item }) => <PostListItem post={item} />}
 			showsVerticalScrollIndicator={false}
 			contentContainerStyle={{ gap: 10 }}
+			onEndReached={loadMore}
+			refreshing={loading}
+			onRefresh={refetch}
 		/>
 	);
 }
